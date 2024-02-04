a636766f294bb8e74b32ab13f29df9bf9c849abc
Eduardo Hernández Oyarzún <eduardo.he095@gmail.com>
2024-02-04 01:46:34 -0300

    Home / Data / Socials

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.linkedin.com/in/eduardohernandezoyarzun/"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i class="uil uil-linkedin"></i>
+      </a>
+      <a
+        href="https://www.instagram.com/optickal/"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i class="uil uil-instagram"></i>
+      </a>
+      <a
+        href="https://github.com/Optickal095"
+        className="home__social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i class="uil uil-github"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
